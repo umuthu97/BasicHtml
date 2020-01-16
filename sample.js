0a65e63bee84b86b3a8a4378563e8ba9f3df43aa
V <V>
2020-01-16 18:47:15 +0530

    Html,Js events

---
+++ sample.js
@@ -0,0 +1,13 @@
+
+
+function myFunction() {
+  var mailidValue = document.getElementById("mailid").value;
+  var passwordValue = document.getElementById("password").value;
+  
+  
+  if (passwordValue === "" || mailidValue === "") {
+    alert("ERROR!");
+    return
+  }
+  alert("SUCCESS!");
+}
